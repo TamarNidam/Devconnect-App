cc32884d13051150c0b2c4f0ac6a297d2eec516e
TamarNidam <tamtam2003n@gmail.com>
2023-11-08 15:52:24 +0200

    Add deploy yaml

--- init.sh
+++ init.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Build Docker image
-docker build -t django-webapp .
+docker build -t tamarnidam-webapp:latest .
 
 docker volume create db
 
 # Create Docker container
 #docker run -it -p 8000:8000 django-webapp
-docker run -d -p 8000:8000 -v db:/app django-webapp
+docker run -d -p 8000:8000 -v db:/app tamarnidam-webapp:latest
 
 # Display container information
 docker ps -a
