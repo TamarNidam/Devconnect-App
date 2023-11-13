251baf4ef3fc6bb981cbe2e47067ca580dc74f55
TamarNidam <tamtam2003n@gmail.com>
2023-11-13 10:57:45 +0200

    כ

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 image="tamarnidam-webapp:1.0"
-artifact_registry="me-west1-docker.pkg.dev/devconnect-final-project/tamar-nidam-artifacts/$image"
+artifact_registry="me-west1-docker.pkg.dev/devconnect-project/tamar-nidam-artifcts/$image"
 gcloud auth configure-docker me-west1-docker.pkg.dev
 docker tag "$image" "$artifact_registry"
 docker push "$artifact_registry"
